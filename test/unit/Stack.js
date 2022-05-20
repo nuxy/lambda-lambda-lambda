d1e0736e21a27b88abe7553185ac1144fcaa97fd
Marc S. Brooks <marc@mbrooks.info>
2022-05-20 14:20:49 -0700

    Expanded testing (check Response data)

--- test/unit/Stack.js
+++ test/unit/Stack.js
@@ -1,16 +1,19 @@
 'use strict';
 
-const chai = require('chai');
+const event = require('../event.json');
+const chai  = require('chai');
 
 const expect = chai.expect;
 
 // Load modules.
-const Common = require('../../src/router/Common.js');
-const Stack  = require('../../src/router/Stack.js');
+const Common   = require('../../src/router/Common.js');
+const Request  = require('../../src/router/Request.js');
+const Response = require('../../src/router/Response.js');
+const Stack    = require('../../src/router/Stack.js');
 
 describe('Stack module', function() {
   describe('Instance methods', function() {
-    describe('add()', function() {
+    describe('add', function() {
       describe('middleware', function() {
         const stack = new Stack();
 
@@ -89,20 +92,70 @@ describe('Stack module', function() {
 
       let count = 0;
 
-      const func = function(req, res, next) {
+      const func1 = function(req, res, next) {
+        res.setHeader('Middleware', true);
         count++;
         next();
       };
 
-      stack.middleware = [func];
-      stack.routes     = [func];
-      stack.resources  = [func];
-      stack.fallback   = func;
+      Common.setFuncName(func1, 'middleware');
 
-      stack.exec({}, {});
+      const func2 = function(req, res, next) {
+        res.setHeader('Route', true);
+        res.status(200).send('Success');
+        count++;
+        next();
+      };
 
-      it('should return value', function() {
+      Common.setFuncName(func1, 'route:get');
+
+      const func3 = function(req, res, next) {
+        res.setHeader('Resource', true);
+        res.status(200).send('Success');
+        count++;
+        next();
+      };
+
+      Common.setFuncName(func1, 'resource:get');
+
+      const func4 = function(req, res, next) {
+        res.setHeader('Fallback', true);
+        res.status(200).send('Success');
+        count++;
+        next();
+      };
+
+      Common.setFuncName(func1, 'fallback');
+
+      stack.middleware = [func1];
+      stack.routes     = [func2];
+      stack.resources  = [func3];
+      stack.fallback   = func4;
+
+      const req = new Request(event.Records[0].cf.request, {});
+      const res = new Response({});
+
+      stack.exec(req, res);
+
+      const result = res.data();
+
+      it('should execute functions', function() {
         expect(count).to.equal(4);
+      });
+
+      it('should return headers', function() {
+        expect(result.headers.middleware).to.be.an('array');
+        expect(result.headers.route).to.be.an('array');
+        expect(result.headers.resource).to.be.an('array');
+        expect(result.headers.fallback).to.be.an('array');
+      });
+
+      it('should return status', function() {
+        expect(result.status).to.equal(200);
+      });
+
+      it('should return body', function() {
+        expect(result.body).to.equal('Success');
       });
     });
   });
