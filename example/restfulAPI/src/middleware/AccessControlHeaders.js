82eea89137dccf938897fc78d6347094cb7da247
Marc S. Brooks <marc@mbrooks.info>
2022-06-24 23:26:05 -0700

    Refactor, added preflight options handling

--- example/restfulAPI/src/middleware/AccessControlHeaders.js
+++ example/restfulAPI/src/middleware/AccessControlHeaders.js
@@ -6,12 +6,19 @@ const config = require('../config.json');
  * Middleware to send Access-Control-* headers.
  */
 module.exports = (req, res, next) => {
-  if (config.development === true) {
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Headers', 'Accept,Content-Type');
-    res.setHeader('Access-Control-Allow-Methods', 'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9000');
-  }
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Headers', 'Accept,Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT');
 
-  next();
+  // Set origin restrictions.
+  res.setHeader('Access-Control-Allow-Origin',
+    (config.development === true) ? config.origin.devUrl : config.origin.siteUrl
+  );
+
+  // Handle preflight options.
+  if (req.method() === 'OPTIONS') {
+    res.status(204).send();
+  } else {
+    next();
+  }
 };
