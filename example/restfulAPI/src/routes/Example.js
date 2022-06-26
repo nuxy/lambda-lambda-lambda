0db6cc7a1a2d0977caf9ec8ff77d0ec6b37d80db
Marc S. Brooks <marc@mbrooks.info>
2022-06-26 15:33:57 -0700

    Added path/resourceId to response body

--- example/restfulAPI/src/routes/Example.js
+++ example/restfulAPI/src/routes/Example.js
@@ -13,34 +13,34 @@ module.exports = {
    * GET /api/example
    */
   index (req, res) {
-    res.status(200).send('index');
+    res.status(200).send('example:index');
   },
 
   /**
    * PUT /api/example/<resourceId>
    */
   create (req, res, id) {
-    res.status(201).send('create');
+    res.status(201).send(`example/${id}:create`);
   },
 
   /**
    * PATCH /api/example/<resourceId>
    */
   update (req, res, id) {
-    res.status(204).send('update');
+    res.status(204).send(`example/${id}:update`);
   },
 
   /**
    * DELETE /api/example/<resourceId>
    */
   delete (req, res, id) {
-    res.status(410).send('delete');
+    res.status(410).send(`example/${id}:delete`);
   },
 
   /**
    * POST /api/example
    */
   submit (req, res) {
-    res.status(200).send('submit');
+    res.status(200).send('example:submit');
   }
 };
