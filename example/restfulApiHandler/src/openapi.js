fe545b01042d818a33536f1a486ccca72a4fe7e9
Marc S. Brooks <marc@mbrooks.info>
2023-02-05 14:21:11 -0800

    Synchronized L³ extension changes

--- example/restfulApiHandler/src/openapi.js
+++ example/restfulApiHandler/src/openapi.js
@@ -1,9 +1,9 @@
 module.exports = {
   openapi: '3.0.0',
   info: {
-    title: 'restfulApiHandler',
-    description: 'Handler example with preconfigured routes and middleware.',
-    version: '0.0.1',
+    title: process.env.npm_package_name,
+    description: process.env.npm_package_description,
+    version: process.env.npm_package_version
   },
   servers: [{
     url: 'http://localhost:3000'
